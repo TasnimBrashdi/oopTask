5bd003bf4f9ac1aa92ea1f0f73e0bce42ad5d6fe
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-25 14:58:13 +0400

    create Two constructors

---
+++ OOP-Task/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task
+{
+    public class BankAccount
+    {
+        private string AccountNumber;
+        private string AccountHolderName;
+        private decimal Balance;
+     public BankAccount(string accountNumber, string accountHolderName)
+        {
+            AccountNumber = accountNumber;
+            AccountHolderName= accountHolderName;
+        }
+      public BankAccount(string accountNumber,string accountHolderName,decimal deposit) {
+
+            AccountNumber = accountNumber;
+            AccountHolderName = accountHolderName;
+            Balance = deposit;
+
+        }
+    }
+    
+
+}
