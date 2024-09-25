a3b492eeccdf7c29b1f7357795a15ed355d69270
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-25 16:04:55 +0400

    Add class Bank and methods

---
+++ OOP-Task/Bank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Task
+{
+    internal class Bank
+    {
+        public List<BankAccount> AccountList = new List<BankAccount>();
+        public void AddAccount(BankAccount account) {
+
+            AccountList.Add(account);
+
+
+        }
+        public void DisplayAllAccounts() { 
+        
+        
+        }
+        public void GetAccountByNumber(BankAccount number) { 
+
+        
+        }
+
+    }
+}
