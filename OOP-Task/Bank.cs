9119ebbbcdc698ad2b50466153649a98862c85b2
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-26 14:10:12 +0400

    update

--- OOP-Task/Bank.cs
+++ OOP-Task/Bank.cs
@@ -1,28 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOP_Task
 {
-    internal class Bank
+    public class Bank
     {
         public List<BankAccount> AccountList = new List<BankAccount>();
-        public void AddAccount(BankAccount account) {
+        public Bank() {
 
+            AccountList.Add(new BankAccount( "325632435","Ahmed Ali", 12));
+            AccountList.Add(new BankAccount( "325625634", "Noor Hamed"));
+            AccountList.Add(new BankAccount("325625634","Fatma Khlid"));
+            AccountList.Add(new BankAccount("325625098","Salim Omr", 100));
+
+
+
+        }
+        public void AddAccount(BankAccount account) {
             AccountList.Add(account);
 
+            Console.WriteLine("Account Added");
+        }
+        public void DisplayAllAccounts() {
+
+            foreach (var acc in AccountList) {
+                acc.GetAccountInfo();
+            }
 
         }
-        public void DisplayAllAccounts() { 
-        
-        
+        public BankAccount GetAccountByNumber(string accountNumber)
+        {
+            foreach (BankAccount account in AccountList)
+            {
+                if (account.GetAccountNumber()== accountNumber)
+                { return account; }
+            }
+            Console.WriteLine("Account not found!");
+            return null;
         }
-        public void GetAccountByNumber(BankAccount number) { 
 
-        
-        }
-
-    }
-}
+    } }
