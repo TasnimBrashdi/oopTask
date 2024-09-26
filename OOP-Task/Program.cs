3962264712324cc42fc00658cdec664a63f08d95
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-26 14:09:19 +0400

    add case

--- OOP-Task/Program.cs
+++ OOP-Task/Program.cs
@@ -2,59 +2,78 @@
 {
     public class Program
     {
+        private static Bank Bank = new Bank();
         static void Main(string[] args)
         {
-            Console.WriteLine("- - - WELCOME TO BANK SYSTEM - - -");
-            Console.WriteLine("- 1-Add Account -");
-            Console.WriteLine("- 2-Display All Accounts -");
-            Console.WriteLine("- 3-Get Account By Number  -");
-            Console.WriteLine("- 4-deposit  -");
-            Console.WriteLine("- 5-Withdraw  -");
-            Console.WriteLine("\n-Enter yOur Chooice -");
-            int c=int.Parse(Console.ReadLine());
+           bool runn=true;
+            try
+            {
+                while (runn)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
 
-            switch (c) { 
-                    case 1:
-                    Bank adding =new Bank();
-                    adding.AddAccount();
-                    break;
-                    case 2:
-                    Bank d = new Bank();
-                    d.DisplayAllAccounts();
-                    break;
-                    case 3:
-                    Console.WriteLine("Enter number account");
-                    string accountNumber = Console.ReadLine();
-                    Bank g = new Bank();
-                    g.GetAccountByNumber(accountNumber);
-                    break;
-                    case 4:
-                    Console.WriteLine("Enter number account");
-                    string accountN = Console.ReadLine();
-                    Console.WriteLine("Enter NAME account");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("Enter number of money");
-                    decimal m =decimal.Parse( Console.ReadLine());
+                    Console.WriteLine("- - - WELCOME TO BANK SYSTEM - - -");
+                    Console.WriteLine("- 1 -Add Account -");
+                    Console.WriteLine("- 2- Display All Accounts -");
+                    Console.WriteLine("- 3- deposit  -");
+                    Console.WriteLine("- 4- Withdraw  -");
+                    Console.WriteLine("- 5- Exit  -");
+                    Console.WriteLine("\n-  Enter yOur Chooice  -");
+                    int c = int.Parse(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.White;
+                    switch (c)
+                    {
 
-                    BankAccount dep = new BankAccount(name,accountN);    
-                    dep.deposit(m);
-                    break;
-                    case 5:
-                    Console.WriteLine("Enter number account");
-                    string accountNw = Console.ReadLine();
-                    Console.WriteLine("Enter NAME account");
-                    string namew = Console.ReadLine();
-                    Console.WriteLine("Enter number of money");
-                    decimal num = decimal.Parse(Console.ReadLine());
+                        case 1:
 
-                    BankAccount w = new BankAccount(namew, accountNw);
-                    w.Withdraw(num);
-                    break;
-                    default:
+                            Console.WriteLine("Enter AccOunt NumBer");
+                            string accountNumber = Console.ReadLine();
 
-                    Console.WriteLine("INVAILD INPUT");
-                    break;
-            
+                            Console.WriteLine("Enter AccOunt name");
+                            string Holdername = Console.ReadLine();
+                           
+                            Bank.AddAccount(new BankAccount( accountNumber, Holdername));
+                            break;
+                        case 2:
+
+                            Bank.DisplayAllAccounts();
+                            break;
+                       
+                        case 3:
+
+                            Console.WriteLine("Enter number account");
+                            string accountN = Console.ReadLine();
+                            Console.WriteLine("Enter number of money");
+                            decimal m = decimal.Parse(Console.ReadLine());
+                            BankAccount account = Bank.GetAccountByNumber(accountN);
+                            account.deposit(m);
+
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter number account");
+                            string accountNw = Console.ReadLine();
+                            
+                            Console.WriteLine("Enter number of money");
+                            decimal num = decimal.Parse(Console.ReadLine());
+
+                            BankAccount accountw = Bank.GetAccountByNumber(accountNw);
+                            accountw.Withdraw(num);
+                            break;
+                        case 5:
+                            runn = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("INVAILD INPUT");
+                            break;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR" + ex.Message);
             }
 
 
