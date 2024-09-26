9045e7b8599a2aee634b562a4008f95a402189b2
Tasnim <tasnimalbrashdi@gmail.com>
2024-09-26 09:46:30 +0400

    Add Menu bank option

--- OOP-Task/Program.cs
+++ OOP-Task/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace OOP_Task
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("- - - WELCOME TO BANK SYSTEM - - -");
+            Console.WriteLine("- 1-Add Account -");
+            Console.WriteLine("- 2-Display All Accounts -");
+            Console.WriteLine("- 3-Get Account By Number  -");
+            Console.WriteLine("- 4-deposit  -");
+            Console.WriteLine("- 5-Withdraw  -");
+            Console.WriteLine("\n-Enter yOur Chooice -");
+            int c=int.Parse(Console.ReadLine());
+
+            switch (c) { 
+                    case 1:
+
+                    break;
+                    case 2:
+
+                    break;
+                    case 3:
+
+                    break;
+                    case 4:
+
+                    break;
+                    case 5:
+
+                    break;
+                    default:
+
+                    Console.WriteLine("INVAILD INPUT");
+                    break;
+            
+            }
+
+
+
+
+
+
         }
     }
+
 }
